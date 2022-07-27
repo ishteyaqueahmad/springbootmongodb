bd1bbc2f0177fd53b323de037a71890e7d7ec6c1
Ishteyaque <ahmad.ishteyaque966@gmail.com>
2022-07-27 23:20:28 +0530

    implemented findStudentByEmail custom method.

--- src/main/java/com/example/springbootmongodb/repository/StudentRepository.java
+++ src/main/java/com/example/springbootmongodb/repository/StudentRepository.java
@@ -3,5 +3,9 @@ package com.example.springbootmongodb.repository;
 import com.example.springbootmongodb.model.Student;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface StudentRepository extends MongoRepository<Student,String> {
+
+    Optional<Student> findStudentByEmail(String email);
 }
