e25266198a8088c381762a35529f90e22f505331
Ishteyaque <ahmad.ishteyaque966@gmail.com>
2022-07-27 23:17:09 +0530

    implement query for finding by email. closes #3.

--- src/main/java/com/example/springbootmongodb/SpringbootmongodbApplication.java
+++ src/main/java/com/example/springbootmongodb/SpringbootmongodbApplication.java
@@ -8,6 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -22,18 +25,28 @@ public class SpringbootmongodbApplication {
 	}
 
 	@Bean
-	CommandLineRunner runner(StudentRepository repository){
+	CommandLineRunner runner(StudentRepository repository,
+							 MongoTemplate mongoTemplate){
 		return args -> {
 			Address address=new Address("India","110025","Delhi");
+			String email = "ahmad@gmail.com";
 			Student student=new Student("Ishteyaque",
 					"Ahmad",
-					"ahmad@gmail.com",
+					email,
 					Gender.MALE,
 					address,
 					List.of("Computer science","Maths"),
 					BigDecimal.TEN,
 					LocalDateTime.now());
 
+			Query query=new Query();
+			query.addCriteria(Criteria.where("email").is(email));
+
+			List<Student> students = mongoTemplate.find(query, Student.class);
+			if (students.size()>1)
+				throw new IllegalStateException("found many students with email"+email);
+
+			if (students.isEmpty())
 			repository.insert(student);
 		};
 
